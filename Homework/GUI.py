0f8aa54d7ccc798ae37d6558f5d64d4432dea5cb
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-17 22:04:15 -0400

    blackjack.py is assignment, Gui.py works

--- Homework/GUI.py
+++ Homework/GUI.py
@@ -8,7 +8,7 @@ class BJ_GUI:
         self.main.title("BlackJack")
 
         self.game = Game()
-
+        
         #bet amounts
         self.bet_label = tk.Label(self.main, text="Bet Amount:", font=("Arial", 16))
         self.bet_label.pack(pady=10)
@@ -23,8 +23,7 @@ class BJ_GUI:
         self.bankroll_label = tk.Label(self.main, text=str(self.game.player), font =("Arial", 16))
         self.bankroll_label.pack(pady=20)
 
-        self.update_display()
-        #setting the frames
+               #setting the frames
         self.dealer_frame = tk.Frame(self.main)
         self.dealer_frame.pack(pady=20)
 
@@ -39,6 +38,8 @@ class BJ_GUI:
         self.player_label = tk.Label(self.player_frame, text="Your Hand", font=("Arial", 16))
         self.player_label.pack()
 
+        self.update_display()
+        
         #Buttons
         self.hit_button = tk.Button(self.main, text="Hit", command=self.hit)
         self.hit_button.pack(side=tk.LEFT, padx=20)
@@ -51,6 +52,7 @@ class BJ_GUI:
 
         self.update_display()
 
+        
     def update_display(self):
         dealer_text = "Dealer's Hand: " + ", ".join(str(card) for card in self.game.dealer.cards)
         player_text = "Your Hand: " + ", ".join(str(card) for card in self.game.player.hand.cards)
@@ -73,32 +75,46 @@ class BJ_GUI:
     def check_winner(self):
         player_val = self.game.player.hand.get_value()
         dealer_val = self.game.dealer.get_value()
+        bet_amount = int(self.bet_entry.get())
 
         if player_val > 21:
             winner = "House"
         elif dealer_val > 21:
             winner = "You, yes YOU!  You Win!!"
+            self.game.player.win(2 * bet_amount)
         elif player_val > dealer_val:
             winner = "Taking that Money, fair and square!!"
+            self.game.player.win(2 * bet_amount)
         elif dealer_val > player_val:
             winner = "House Wins, better luck next time!!"
         else:
             winner = "It's a Tie!"
+            self.game.player.win(bet_amount)
 
+        self.bankroll_label.config(text=str(self.game.player))
         messagebox.showinfo("BlackJack", f"{winner} Win!")
         self.new_game()
 
     def place_bet_start_new(self):
         bet_amount = int(self.bet_entry.get())
-        if not self.game.player.bet(bet_amount):
-            messagebox.showinfo("Blackjack", "Insufficient Funds!")
+        if self.game.player.bankroll <= 0:
+            messagebox.showinfo("BlackJack", "You're out of money! Game Over - loan department is down the hall, and to the right")
+            self.main.quit()
+        elif not self.game.player.bet(bet_amount):
+            messagebox.showinfo("BlackJack", "Insufficient Funds!")
         else:
             self.new_game()
+            
+        # bet_amount = int(self.bet_entry.get())
+        # if not self.game.player.bet(bet_amount):
+        #     messagebox.showinfo("Blackjack", "Insufficient Funds!")
+        # else:
+        #     self.new_game()
 
-    # def new_game(self):
-    #     self.game.start_new_game()
-    #     self.update_display()
-    #     self.bankroll_label.config(text=str(self.game.player))
+    def new_game(self):
+        self.game.start_new_game()
+        self.update_display()
+        self.bankroll_label.config(text=str(self.game.player))
 
         # self.game = Game()
         # for _ in range(2):
